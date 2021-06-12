3695ef953b9da8b438338031ce4b1f44d2ee9aff
Alex Turytsa <italexx.ua@gmail.com>
2021-06-12 14:50:48 +0900

    adding carousel to gamelist

--- App/Screens/Game/GameList.js
+++ App/Screens/Game/GameList.js
@@ -8,34 +8,54 @@ import {
 } from 'react-native';
 import SafeAreaView from 'react-native-safe-area-view';
 import Separator from '../../Components/Separator';
+import Carousel from 'react-native-snap-carousel';
 
 import styles from './GameListStyle';
-
 // import { useGlobals } from '../../contexts/Global';
 
 const GameList = ({ navigation }) => {
-  const [musics, setMusics] = React.useState([
+  const [carouselItems, setCarouselItems] = React.useState([
     {
-      id: '1',
-      url:
-        'https://audio-previews.elements.envatousercontent.com/files/103682271/preview.mp3',
-      type: 'default',
-      title: 'My Title',
-      album: 'My Album',
-      artist: 'Rohan Bhatia',
-      artwork: 'https://picsum.photos/100',
+      title: "Item 1",
+      text: "Text 1",
     },
     {
-      id: '2',
-      url:
-        'https://audio-previews.elements.envatousercontent.com/files/103682271/preview.mp3',
-      type: 'default',
-      title: 'New Title',
-      album: 'New Album',
-      artist: 'Liu Xing',
-      artwork: 'https://picsum.photos/100',
-    }
+      title: "Item 2",
+      text: "Text 2",
+    },
+    {
+      title: "Item 3",
+      text: "Text 3",
+    },
+    {
+      title: "Item 4",
+      text: "Text 4",
+    },
+    {
+      title: "Item 5",
+      text: "Text 5",
+    },
   ]);
+
+  let [activeIndex, setActiveIndex] = React.useState(0);
+
+  const _renderItem = ({ item, index }) => {
+    return (
+      <View style={{
+        backgroundColor: 'floralwhite',
+        borderRadius: 5,
+        height: 250,
+        padding: 50,
+        marginLeft: 25,
+        marginRight: 25,
+      }}>
+        <Text style={{ fontSize: 30 }}>{item.title}</Text>
+        <Text>{item.text}</Text>
+      </View>
+
+    )
+  }
+
   return (
     <SafeAreaView style={[styles.container]}>
       <StatusBar
@@ -44,6 +64,16 @@ const GameList = ({ navigation }) => {
         backgroundColor="#FFFFFF"
         translucent={true}
       />
+      <View style={{ flex: 1, flexDirection: 'row', justifyContent: 'center', }}>
+        <Carousel
+          layout={"default"}
+          ref={ref => this.carousel = ref}
+          data={carouselItems}
+          sliderWidth={300}
+          itemWidth={300}
+          renderItem={_renderItem}
+          onSnapToItem={index => setActiveIndex(index)} />
+      </View>
     </SafeAreaView>
   );
 };
