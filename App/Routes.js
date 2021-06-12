f50244ee47d26daaacabfaf1ceff8baadd8c4419
Vraj <vdesai18@stevens.edu>
2021-06-12 14:52:58 +0900

    stage

--- App/Routes.js
+++ App/Routes.js
@@ -1,8 +1,17 @@
 import * as React from 'react';
-import {StyleSheet} from 'react-native';
+import {
+  Provider as PaperProvider,
+  DefaultTheme,
+  DarkTheme,
+} from 'react-native-paper';
+import {StyleSheet, I18nManager} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
+import { AppearanceProvider, useColorScheme } from 'react-native-appearance';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
+
+import { PreferencesContext } from './contexts/preferencesContext';
+
 import Main from './Screens/Main';
 import SignIn from './Screens/SignIn';
 import SignUp from './Screens/SignUp';
@@ -10,8 +19,44 @@ import SignUp from './Screens/SignUp';
 const Stack = createStackNavigator();
 
 const Routes = () => {
+    const colorScheme = useColorScheme();
+    const [theme, setTheme] = React.useState(
+        colorScheme === 'dark' ? 'dark' : 'light'
+    );
+    const [rtl] = React.useState(I18nManager.isRTL);
+    function toggleTheme() {
+      setTheme(theme => (theme === 'light' ? 'dark' : 'light'));
+    }
+    const toggleRTL = React.useCallback(() => {
+      I18nManager.forceRTL(!rtl);
+      Updates.reloadFromCache();
+    }, [rtl]);
+    const preferences = React.useMemo(
+      () => ({
+        toggleTheme,
+        toggleRTL,
+        theme,
+        rtl: (rtl ? 'right' : 'left'),
+      }),
+      [rtl, theme, toggleRTL]
+    );
     return(
-        <SafeAreaProvider>
+      <SafeAreaProvider>
+        <AppearanceProvider>
+          <PreferencesContext.Provider value={preferences}>
+            <PaperProvider
+                theme={
+                theme === 'light'
+                    ? {
+                        ...DefaultTheme,
+                        colors: { ...DefaultTheme.colors, primary: '#1ba1f2' },
+                    }
+                    : {
+                        ...DarkTheme,
+                        colors: { ...DarkTheme.colors, primary: '#1ba1f2' },
+                    }
+                }
+            >
             <NavigationContainer>
             <Stack.Navigator headerMode="none">
                 <Stack.Screen name="SignIn" component={SignIn} />
@@ -19,7 +64,10 @@ const Routes = () => {
                 <Stack.Screen name="Main" component={Main} />
             </Stack.Navigator>
             </NavigationContainer>
-        </SafeAreaProvider>
+            </PaperProvider>
+          </PreferencesContext.Provider>
+        </AppearanceProvider>
+      </SafeAreaProvider>
     );
 }
 
